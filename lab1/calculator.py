f9df3fba8c9a5e52260abb9f8e08d99bd12ea7dc
Patryk Bober <boberp26@gmail.com>
2021-11-27 21:02:25 +0100

    Add FooCalculator

---
+++ lab1/calculator.py
@@ -0,0 +1,26 @@
+# calculator.py
+
+def sum(m, n):
+    result = m
+    if n < 0:
+        for i in range(abs(n)):
+            result -= 1
+    else:
+        for i in range(n):
+            result += 1
+    return result
+
+
+def divide(m, n):
+    result = 0
+    negative_result = m > 0 and n < 0 or m < 0 and n > 0
+    n = abs(n)
+    m = abs(m)
+
+    while m - n >= 0:
+        m -= n
+        result += 1
+
+    result = -result if negative_result else result
+
+    return result
