06a1321293aab853932792bd1ad815626487f4aa
Patryk Bober <boberp26@gmail.com>
2021-11-27 21:37:04 +0100

    Add division by zero handling

--- lab1/calculator.py
+++ lab1/calculator.py
@@ -17,6 +17,9 @@ def divide(m, n):
     n = abs(n)
     m = abs(m)
 
+    if n == 0:
+        raise ZeroDivisionError('You cannot divide by 0!')
+
     while m - n >= 0:
         m -= n
         result += 1
