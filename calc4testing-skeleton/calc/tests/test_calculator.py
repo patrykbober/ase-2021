4a84e49994b791c8133bb283f6f21fe0646073d6
Patryk Bober <boberp26@gmail.com>
2022-01-12 21:28:48 +0100

    extend tests and fix bugs

--- calc4testing-skeleton/calc/tests/test_calculator.py
+++ calc4testing-skeleton/calc/tests/test_calculator.py
@@ -1,8 +1,6 @@
 from calc.calculator import calculator as c
 import unittest
 
-# TODO: Extend these unit tests for the calculator module!
-
 
 class TestAdd(unittest.TestCase):
     def test_add_integers_positive(self):
@@ -59,6 +57,62 @@ class TestAdd(unittest.TestCase):
 
     def test_divide_by_zero(self):
         self.assertRaises(ZeroDivisionError, c.divide, 6, 0)
+
+    def test_multiply_integers_positive(self):
+        result = c.multiply(3, 6)
+        self.assertEqual(result, 18)
+
+    def test_multiply_integers_negative(self):
+        result = c.multiply(-3, -6)
+        self.assertEqual(result, 18)
+
+    def test_multiply_integers_pos_neg(self):
+        result = c.multiply(3, -6)
+        self.assertEqual(result, -18)
+
+    def test_multiply_integers_neg_pos(self):
+        result = c.multiply(-3, 6)
+        self.assertEqual(result, -18)
+
+    def test_multiply_by_one1(self):
+        result = c.multiply(1, 3)
+        self.assertEqual(result, 3)
+
+    def test_multiply_by_one2(self):
+        result = c.multiply(3, 1)
+        self.assertEqual(result, 3)
+
+    def test_multiply_by_zero1(self):
+        result = c.multiply(0, 3)
+        self.assertEqual(result, 0)
+
+    def test_multiply_by_zero2(self):
+        result = c.multiply(3, 0)
+        self.assertEqual(result, 0)
+
+    def test_subtract_integers_positive(self):
+        result = c.subtract(5, 3)
+        self.assertEqual(result, 2)
+
+    def test_subtract_integers_positive2(self):
+        result = c.subtract(3, 5)
+        self.assertEqual(result, -2)
+
+    def test_subtract_integers_negative(self):
+        result = c.subtract(-5, -3)
+        self.assertEqual(result, -2)
+
+    def test_subtract_integers_negative2(self):
+        result = c.subtract(-3, -5)
+        self.assertEqual(result, 2)
+
+    def test_subtract_integers_pos_neg(self):
+        result = c.subtract(5, -3)
+        self.assertEqual(result, 8)
+
+    def test_subtract_integers_neg_pos(self):
+        result = c.subtract(-3, 5)
+        self.assertEqual(result, -8)
 
     if __name__ == "__main__":
         unittest.main()
